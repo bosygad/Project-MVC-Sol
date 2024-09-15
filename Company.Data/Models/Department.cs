b324d0c9bb115d187750514391adab6b8e076213
bossyGad <bosygad179@gmail.com>
2024-09-15 04:19:37 +0300

    DAL For Department Module

---
+++ Company.Data/Models/Department.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.DAL.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+      
+        public string? Code { get; set; }
+        public string? Name { get; set; }
+
+        public DateTime DataOfCreation {  get; set; } 
+
+    }
+}
